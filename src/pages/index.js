7bdb0a6d85a13d099d73c29c071d57dc3257e380
lukemcgovern <lmcgover@uvm.edu>
2024-12-05 19:33:35 -0500

    changing urls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as Home } from './tarkov-app/Home';
-export { default as Profile } from './tarkov-app/Login';
-export { default as Register } from './tarkov-app/Register';
-export { default as ProtectedPage } from './tarkov-app/Protected';
+export { default as Home } from './Home';
+export { default as Profile } from './Login';
+export { default as Register } from './Register';
+export { default as ProtectedPage } from './Protected';
 export { default as NoPage } from './NoPage';
 export { default as Portfolio } from './Portfolio';
 export { default as ResumePage } from './Resume';
