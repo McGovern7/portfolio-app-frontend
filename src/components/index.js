70ddae15e67c52e10c50e58ef2126a3f3b47dbeb
lukemcgovern <lmcgover@uvm.edu>
2024-12-06 21:09:45 -0500

    remove file types

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-export { default as Button } from './Button.tsx';
-export { default as Card } from './Card.tsx';
-export { default as Grid } from './Grid.tsx';
-export { default as ImageComp } from './ImageComp.tsx';
-export { default as Navbar } from './Navbar.jsx';
-export { default as ScrollTo } from './ScrollTo.tsx';
-export { default as SideBar } from './SideBar.jsx';
-export { default as VerifyToken } from './Verify.jsx';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Grid } from './Grid';
+export { default as ImageComp } from './ImageComp';
+export { default as Navbar } from './Navbar';
+export { default as ScrollTo } from './ScrollTo';
+export { default as SideBar } from './SideBar';
+export { default as VerifyToken } from './Verify';
